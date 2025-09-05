b63c3a4f830c3edfbc92fed7c1ad960d55984501
Daniel Morillo <danifromecuador@gmail.com>
2025-09-05 00:40:07 -0500

    style(app/ui/dashboard/nav-links.tsx): improve UI by showing active link

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,7 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -14,6 +17,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+  const navLinksClass = 'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3';
   return <>
     {links.map((link) => {
       const LinkIcon = link.icon;
@@ -21,7 +26,10 @@ export default function NavLinks() {
         <Link
           key={link.name}
           href={link.href}
-          className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          className={clsx(
+             navLinksClass,
+            { 'bg-sky-100 text-blue-600': pathname === link.href, },
+          )}
         >
           <LinkIcon className="w-6" />
           <p className="hidden md:block">{link.name}</p>
