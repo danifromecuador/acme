7594c1ac169eb73881d7063fa38a1210d4871231
Daniel Morillo <danifromecuador@gmail.com>
2025-09-05 00:15:29 -0500

    fix(app/layout.tsx): typo error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
 
-export default function RootLayout(
-  { children, }: { children: React.ReactNode; }) {
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
       <body
