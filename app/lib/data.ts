a6b5a3133a63049c0827a4ead2e3b598e4f369c6
Daniel Morillo <danifromecuador@gmail.com>
2025-09-19 21:00:20 -0500

    chore(app/lib/data): simulate a slow data fetch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,10 +14,14 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function fetchRevenue() {
   try {
+    console.log("Fetching revenue data...");
     await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+    console.log("Data fetch completed after 3 seconds");
+
     return data;
   } catch (error) {
+    console.error("Database Error:", error);
     throw new Error("Failed to fetch revenue data.");
   }
 }
@@ -30,8 +34,11 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-    const latestInvoices = data.map(invoice=> ({...invoice,amount: formatCurrency(invoice.amount),}));
-    
+    const latestInvoices = data.map((invoice) => ({
+      ...invoice,
+      amount: formatCurrency(invoice.amount),
+    }));
+
     return latestInvoices;
   } catch (error) {
     console.error("Database Error:", error);
